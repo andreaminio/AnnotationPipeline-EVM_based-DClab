c595105db8da78eaf1f82b17b5878e262d989554
Andrea Minio <33559900+andreaminio@users.noreply.github.com>
2021-07-20 23:12:32 -0700

    Update doMap.HISAT2.SR.sh

--- Scripts/doMap.HISAT2.SR.sh
+++ Scripts/doMap.HISAT2.SR.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export PATH=$(realpath /DATA7/Resources/Tools/samtools-1.3.1/):$PATH
+export PATH=$(realpath Tools/samtools-1.3.1/):$PATH
 
-/DATA7/Resources/Tools/hisat2-2.1.0/hisat2 -x $1 -U $2 --very-sensitive -t -p $4 2>err | samtools view -bS -T $1 - | samtools sort -l 9 -@ $4 -m 1500M -o $3 -
+Tools/hisat2-2.1.0/hisat2 -x $1 -U $2 --very-sensitive -t -p $4 2>err | samtools view -bS -T $1 - | samtools sort -l 9 -@ $4 -m 1500M -o $3 -
 
 samtools index $3
